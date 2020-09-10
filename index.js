d74b6872bb71111610adaf7efff2b973a64a8b8c
Paul Schnetzer <1998sheldon@gmail.com>
2020-09-10 14:05:31 +0200

    Final Versio (minimal requirements)

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+const download = require('image-downloader');
 const request = require('request-promise');
 const cheerio = require('cheerio');
 // download the required libaries
+const fs = require('fs');
+
+fs.mkdir('./memes', function (err) {
+  if (err) {
+    console.log('the folder already exitst');
+  } else {
+    console.log('New directory successfully created.');
+  }
+});
 (async () => {
   const memeURL = 'https://memegen.link/examples';
 
@@ -8,13 +18,30 @@ const cheerio = require('cheerio');
 
   const $ = cheerio.load(response);
   let memeURLArray = [];
-  for (let i = 1; i < 11; i++) {
+  memeURLArray.push(
+    'https://api.memegen.link/images/bender/your_text/goes_here.jpg?preview=true&watermark=none',
+  );
+  for (let i = 1; i < 10; i++) {
     let name = $(
       'body > div.meme-templates > div.row > div:nth-child(' +
         i +
         ') > a > img',
     ).attr('src');
-    memeURLArray.push(name);
+    memeURLArray.push('https://api.memegen.link/images' + name);
   }
+
   console.log(memeURLArray);
+  for (let i = 0; i < 11; i++) {
+    const options = {
+      url: memeURLArray[i],
+      dest: './memes/image' + i + '.png', // will be saved to /path/to/dest/image.jpg
+    };
+
+    download
+      .image(options)
+      .then(({ filename }) => {
+        console.log('Saved to', filename); // saved to /path/to/dest/image.jpg
+      })
+      .catch((err) => console.error(err));
+  }
 })();
